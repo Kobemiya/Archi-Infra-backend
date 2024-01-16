2b3b964ab3c06b6c8e4ff1f8feb4d18fc000d338
Romain Barbier <romain.barbier@epita.fr>
2024-01-17 00:17:11 +0100

    try to deploy

---
+++ src/Archi-Infra-backend/Models/Todo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace InfraArchi;
+
+public partial class Todo
+{
+    [JsonPropertyName("pk")]
+    public int Pk => Id;
+
+    [JsonPropertyName("sk")]
+    public int Sk => Id;
+    public int Id { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Name { get; set; }
+}
---
+++ src/Archi-Infra-backend/Models/TodoDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InfraArchi.Models;
+
+public partial class TodoDbContext : DbContext
+{
+    private string? _connectionString = System.Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+    
+    public TodoDbContext()
+    {
+    }
+
+    public TodoDbContext(DbContextOptions<TodoDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Todo> Todos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql(_connectionString);
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Todo>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("todo_pk");
+
+            entity.ToTable("todo");
+
+            entity.Property(e => e.Id)
+                .HasColumnName("id");
+            entity.Property(e => e.Description)
+                .HasColumnType("character varying")
+                .HasColumnName("description");
+            entity.Property(e => e.Name)
+                .HasColumnType("character varying")
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
